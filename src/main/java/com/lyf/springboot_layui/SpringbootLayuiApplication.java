0f818d53fc100f78899e82cf9ac0bec2b29494ab
lyf <2025502807@qq.com>
2018-09-19 11:46:01 +0800

    第一次提交

---
+++ src/main/java/com/lyf/springboot_layui/SpringbootLayuiApplication.java
@@ -0,0 +1,12 @@
+package com.lyf.springboot_layui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLayuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLayuiApplication.class, args);
+	}
+}
